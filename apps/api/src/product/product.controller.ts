b4d280d09ca13f65c78f171bd3f4af12a10d8e3a
Pablojns <pablovisk2.0@gmail.com>
2026-06-24 12:48:51 -0300

    feat: permissao desativar produto

--- apps/api/src/product/product.controller.ts
+++ apps/api/src/product/product.controller.ts
@@ -62,6 +62,7 @@ export class ProductController {
   }
 
   @Delete(':id')
+  @RequirePermission('estoque', 'desativar_produto')
   remove(
     @Param('id', ParseUUIDPipe) id: string,
     @CurrentUser() user: AuthUser,
